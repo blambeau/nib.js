ef8fe1702e7b6bd3ac4bb1ce223198437a620870
Bernard Lambeau <blambeau@gmail.com>
2011-02-21 18:13:04 +0100

    Added --coffee and --uglify options

--- lib/nibjs/main.rb
+++ lib/nibjs/main.rb
@@ -13,39 +13,122 @@ module NibJS
   #
   class Main < Quickl::Command(__FILE__, __LINE__)
     
+    attr_accessor :coffee
+    attr_accessor :uglify
+    
     # Install options
     options do |opt|
+      @coffee = false
+      opt.on("-c", "--coffee", "Specify if sources are in coffeescript") do
+        @coffee = true
+      end
+      @uglify = false
+      opt.on('-u', '--[no-]uglify', "Minimize generated javascript (requires uglifyjs)") do |value| 
+        @uglify = value
+      end
       opt.on_tail("--help", "Show help") do
         raise Quickl::Help
       end
       opt.on_tail("--version", "Show version") do
         raise Quickl::Exit, "#{program_name} #{NibJS::VERSION} (c) 2011, Bernard Lambeau"
-      end
+      end      
     end
+    
+    ### In coffee
       
-    def with_registration(file)
+    def with_coffee_registration(file)
       code = ""
       code += "nibjs.register '#{file}', (exports, require)->\n"
       code += yield.gsub(/^/m, '  ')
       code += "\n\n"
     end
 
-    def with_nib_define(package)
+    def with_coffee_define(package)
       code = ""
       code += "NibJS.define '#{package}', (nibjs)->\n"
       code += yield.gsub(/^/m, '  ')
       code += "\n"
-      code += "  nibjs.require './index'"
+      code += "  nibjs.require './index'\n"
+    end
+    
+    ### In javascript
+    
+    def with_js_registration(file)
+      code = ""
+      code += "nibjs.register('#{file}', function(exports, require){\n"
+      code += yield.gsub(/^/m, '  ')
+      code += "});\n\n"
+    end
+
+    def with_js_define(package)
+      code = ""
+      code += "NibJS.define('#{package}', function(nibjs){\n"
+      code += yield.gsub(/^/m, '  ')
       code += "\n"
+      code += "  nibjs.require('./index');\n"
+      code += "});\n"
+    end
+    
+    ###
+    
+    # Runs a command, returns result on STDOUT. If the exit status was no 0,
+    # a RuntimeError is raised. 
+    def safe_run(cmd)
+      res = `#{cmd}`
+      unless $?.exitstatus == 0
+        raise RuntimeError, "Error while executing #{cmd}" 
+      end
+      res
+    end
+    
+    def with_temp_file(content)
+      require "tempfile"
+      file = Tempfile.new('nibjs')
+      file << content
+      file.close
+      res = yield(file)
+      file.unlink
+      res
+    end
+    
+    ###
+    
+    def files(folder)
+      if coffee
+        Dir["#{folder}/**/*.coffee"]
+      else
+        Dir["#{folder}/**/*.js"]
+      end
+    end
+
+    def coffee_compile(code)
+      with_temp_file(code){|f|
+        safe_run("cat #{f.path} | coffee --compile --stdio --bare #{f.path}")
+      }
     end
 
     def compile(folder)
-      puts with_nib_define(File.basename(folder)){
-        Dir["#{folder}/**/*.coffee"].collect{|file|
-          rel = file[(1+folder.size)..-8]
-          with_registration("./#{rel}"){ File.read(file) }
-        }.join("\n")
-      }
+      code = if coffee
+        code = with_coffee_define(File.basename(folder)){
+          files(folder).collect{|file|
+            rel = file[(1+folder.size)..-8]
+            with_coffee_registration("./#{rel}"){ File.read(file) }
+          }.join("\n")
+        }
+        coffee_compile(code)
+      else
+        with_js_define(File.basename(folder)){
+          files(folder).collect{|file|
+            rel = file[(1+folder.size)..-8]
+            with_js_registration("./#{rel}"){ File.read(file) }
+          }.join("\n")
+        }
+      end
+      if uglify
+        with_temp_file(code){|f| safe_run("uglifyjs #{f.path}") }
+      else
+        code
+      end
     end
     
     def with_output
--- tasks/spec_test.rake
+++ tasks/spec_test.rake
@@ -2,7 +2,7 @@ begin
 
   desc "Build spec fixtures from coffee to js"
   task :build_spec_fixture do
-    shell_safe_exec("ruby ./bin/nibjs spec/fixture | coffee --compile --bare --stdio > spec/fixture.js")
+    shell_safe_exec("./bin/nibjs --coffee --uglify spec/fixture > spec/fixture.js")
   end
   
   desc "Run spec examples"
