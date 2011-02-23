f22a501f3510f24d41f91c34a881a23744dd4f08
Bernard Lambeau <blambeau@gmail.com>
2011-02-23 21:07:38 +0100

    Regenerated doc.

--- 1-basic/lib/App.js
+++ 1-basic/lib/App.js
@@ -2,7 +2,7 @@ var StringUtils = require('./StringUtils').StringUtils;
 exports.App = {
   
   runTests: function(){
-    $("body").append(StringUtils.pre("nibjs --output jsapp.js lib"));
+    $("body").append(StringUtils.pre("nibjs --libname=jsapp --output=jsapp.js lib"));
     return true;
   }
   
